ae6dfc3d20e7a0e25dd279c17373d80033747d58
zhangyangzhong <ococo09000@gmail.com>
2021-03-14 17:34:06 +0800

    finish

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,77 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <form @submit.prevent="AddFn">
+        <p style="text-align: center">TodoList</p>
+        <input type="text" v-model="inputValue" placeholder="list" autofocus>
+        <p>{{inputValue}}</p>
+        <button>Add to the List</button>
+        <p id="loopList" v-for=" (List,idx) in todo.list" :key="idx" @click="removeList(idx)">{{idx+1}}.{{List}}</p>
+      </form>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { reactive, ref } from 'vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  setup(){
+
+    const inputValue = ref('')
+    // using reactive is best for datas
+    const todo = reactive({
+      list:[],
+      })
+
+    const AddFn = () => {
+      todo.list.push(inputValue.value)
+      inputValue.value = ""
+    }
+
+    const removeList = (parameter) => {
+      return todo.list.splice(parameter,1),
+      (todo.list.length == 0)?alert("finish todolist"):null
+    }
+    
+    return{
+      inputValue,
+      AddFn,
+      todo,
+      removeList,
+    }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body,html{
+  padding: 0;
+  margin: 0;
+  user-select: none;
 }
+  .container{
+    box-sizing: border-box;
+    padding: 10px;
+    display: flex;
+    justify-content: center;
+    background-color: rgb(221, 221, 221);
+    align-items: center;
+    height: 100vh;
+    width: 100%;
+    #loopList{
+      display: block;
+      cursor: pointer;
+    }
+    input{
+      outline: none;
+    }
+    button{
+      width: 100%;
+      padding: 5px 0 5px 0;
+      border-radius: 5px;
+      outline: none;
+      border: none;
+      background-color: rgb(39, 118, 182);
+      color: white;
+      cursor: pointer;
+    }
+  }
 </style>
